fff047540cdb64549007c65ce676b00d4444df6e
c3k4ah <dominickp20.aps1b@gmail.com>
2022-02-04 11:03:47 +0300

    textfield multilines

--- lib/views/new_note.dart
+++ lib/views/new_note.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:note_book/views/widgets/colors.dart';
 
 class NewNote extends StatelessWidget {
   const NewNote({Key? key}) : super(key: key);
@@ -8,9 +9,7 @@ class NewNote extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SizedBox(
-        width: Get.width,
-        height: Get.height,
+      body: SingleChildScrollView(
         child: Column(
           children: [
             const SizedBox(
@@ -21,6 +20,13 @@ class NewNote extends StatelessWidget {
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
+                  IconButton(
+                      iconSize: 30,
+                      onPressed: () {},
+                      icon: const Icon(
+                        Iconsax.trush_square,
+                        color: Colors.red,
+                      )),
                   const Text(
                     "Create",
                     style: TextStyle(
@@ -28,16 +34,59 @@ class NewNote extends StatelessWidget {
                         fontSize: 30,
                         fontWeight: FontWeight.normal),
                   ),
-                  Row(
-                    children: [
-                      IconButton(
-                          onPressed: () {}, icon: const Icon(Iconsax.trash)),
-                      IconButton(
-                          onPressed: () {},
-                          icon: const Icon(Iconsax.tick_square)),
-                    ],
-                  ),
+                  IconButton(
+                      iconSize: 30,
+                      onPressed: () {},
+                      icon: const Icon(
+                        Iconsax.tick_square,
+                        color: Colors.blue,
+                      )),
                 ],
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 10),
+              decoration: BoxDecoration(
+                  color: blueAccent.withOpacity(.5),
+                  borderRadius: BorderRadius.circular(7)),
+              margin: const EdgeInsets.symmetric(horizontal: 15),
+              width: Get.width,
+              height: 40,
+              child: const Expanded(
+                child: TextField(
+                  decoration: InputDecoration.collapsed(
+                    border: InputBorder.none,
+                    hintText: '',
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.all(15),
+              width: Get.width,
+              height: 50,
+              decoration: BoxDecoration(
+                  color: Colors.blue, borderRadius: BorderRadius.circular(7)),
+              child: Row(),
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 10),
+              decoration: BoxDecoration(
+                  color: blueAccent.withOpacity(.5),
+                  borderRadius: BorderRadius.circular(7)),
+              margin: const EdgeInsets.symmetric(horizontal: 15),
+              width: Get.width,
+              height: Get.height * .55,
+              child: const Expanded(
+                child: TextField(
+                  keyboardType: TextInputType.multiline,
+                  maxLength: null,
+                  maxLines: null,
+                  decoration: InputDecoration.collapsed(
+                    border: InputBorder.none,
+                    hintText: '',
+                  ),
+                ),
               ),
             ),
           ],
