17265379a2ef2168b9cc379211fa767b8e4dddb8
c3k4ah <dominickp20.aps1b@gmail.com>
2022-02-09 04:12:13 +0300

    reade page done

--- lib/views/page_lecture.dart
+++ lib/views/page_lecture.dart
@@ -10,6 +10,9 @@ class ReadPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    String lorem =
+        "Contrairement à une opinion répandue, le Lorem Ipsum n'est pas simplement du texte aléatoire. Il trouve ses racines dans une oeuvre de la littérature latine classique datant de 45 av. J.-C., le rendant vieux de 2000 ans. Un professeur du Hampden-Sydney College, en Virginie, s'est intéressé à un des mots latins les plus obscurs, consectetur, extrait d'un passage du Lorem Ipsum, et en étudiant tous les usages de ce mot dans la littérature classique, découvrit la source incontestable du Lorem Ipsum. Il provient en fait des sections 1.10.32 et 1.10.33 du Des Suprêmes Biens et des Suprêmes Maux) de Cicéron. Cet ouvrage, très populaire pendant la Renaissance, est un traité sur la théorie de l'éthique. Les premières lignes du Lorem Ipsum,  proviennent de la section 1.10.32.L'extrait standard de Lorem Ipsum utilisé depuis le XVIè siècle est reproduit ci-dessous pour les curieux. Les sections 1.10.32 et 1.10.33 du de Cicéron sont aussi reproduites dans leur version originale, accompagnée de la traduction anglaise de H. Rackham (1914).";
+
     return Scaffold(
       backgroundColor: Colors.black,
       appBar: AppBar(
@@ -52,15 +55,37 @@ class ReadPage extends StatelessWidget {
             color: Colors.white,
             borderRadius: BorderRadius.only(
                 topLeft: Radius.circular(40), topRight: Radius.circular(40))),
-        child: Column(
-          mainAxisSize: MainAxisSize.max,
+        child: Stack(
           children: [
+            Expanded(
+              child: Flex(direction: Axis.vertical, children: [
+                Container(
+                  padding: EdgeInsets.all(15),
+                  width: Get.width,
+                  height: Get.height * .8,
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        Text(
+                          lorem + lorem + lorem,
+                          overflow: TextOverflow.fade,
+                          style: TextStyle(color: Colors.black, fontSize: 16),
+                        ),
+                        SizedBox(
+                          height: Get.height * .05,
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ]),
+            ),
             Expanded(
               child: Align(
                 alignment: Alignment.bottomCenter,
                 child: Container(
                   padding: EdgeInsets.symmetric(horizontal: 7),
-                  margin: EdgeInsets.symmetric(vertical: 15),
+                  margin: EdgeInsets.symmetric(vertical: 5),
                   width: Get.width * .8,
                   height: 50,
                   decoration: BoxDecoration(
